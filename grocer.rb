8cf24ecea8e616027abcffefec69de51c388624c
SOYEONG OH <axionoso@gmail.com>
2019-03-29 17:26:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -58,6 +58,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  consolidate_cart(cart)
-  apply_coupons(cart)
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart)
+  cart = apply_clearance(cart)
 end
